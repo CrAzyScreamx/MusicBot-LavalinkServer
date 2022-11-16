b3c1dfea7c7bf8b7252b12061d91fa5866ec872d
CrAzyScreamx <34142944+CrAzyScreamx@users.noreply.github.com>
2022-11-16 20:24:47 +0200

    Added gitignore

---
+++ configs/managers/databaseManager.py
@@ -0,0 +1,10 @@
+import mysql.connector as mysql
+import os
+
+
+class databaseManager:
+
+    def __init__(self):
+        self.host = os.getenv("DB_HOST")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASS")
